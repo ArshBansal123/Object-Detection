bcbdedb2ad45191a5c2c09a1618621ecec64ce73
ArshBansal123 <76432668+ArshBansal123@users.noreply.github.com>
2021-12-04 14:25:09 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {
+    img = loadImage('dog_cat.jpg');
+}
+function setup() {
+    canvas = createCanvas ( 640, 420);
+    canvas.center();
+}
+function draw(){
+    Image(img, 0, 0, 640, 420);
+    fill(255, 0, 0);
+    text("Dog", 45, 75);
+    noFill();
+    stroke(255, 0, 0);
+    rect(30, 60, 450, 350);
+    rect(250, 60, 450, 350);
+  
+}
